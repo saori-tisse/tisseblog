ca1d4ba5c2ca0ef64e91e03c7e949f48c6ce3baf
saori-tisse <stera10@live.jp>
2020-03-27 17:03:59 +0900

    開発準備

---
+++ pages/blog/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ item.title }}</h2>
+    <div v-html="item.body"></div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://tisseblog.microcms.io/api/v1/blog/${params.id}`,
+      {
+        headers: { "X-API-KEY": process.env.API_KEY }
+      }
+    );
+    return {
+      item: data
+    };
+  }
+};
+</script>
