6151cc4fed286d4b81b328a635288cdd21a15a7b
saori-tisse <stera10@live.jp>
2020-03-30 10:47:04 +0900

    _id.vue 編集

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -3,9 +3,14 @@
     <div>
       <div class="post-meta">
         <div class="thubcatch" v-if="item.icatch">{{item.icatch}}</div>
-        <h2>{{ item.title }}</h2>
-        <p class="tile">
-          <time>{{item.createdAt}}</time>
+        <h1>{{ item.title }}</h1>
+        <p class="date -create">
+          <i class="far fa-calendar"></i>
+          <time :datetime="item.createdAt">{{item.createdAt | moment}}</time>
+        </p>
+        <p class="date -update" v-if="item.updatedAt">
+          <i class="fas fa-sync-alt"></i>
+          <time :datetime="item.updatedAt">{{item.updatedAt | moment}}</time>
         </p>
       </div>
       <div class="post-contents">
@@ -17,6 +22,7 @@
 
 <script>
 import axios from "axios";
+import moment from "moment";
 
 export default {
   async asyncData({ params }) {
@@ -29,6 +35,11 @@ export default {
     return {
       item: data
     };
+  },
+  filters: {
+    moment: function(date) {
+      return moment(date).format("YYYY/MM/DD HH:mm");
+    }
   }
 };
 </script>
@@ -41,23 +52,52 @@ export default {
   &-wrap {
     margin-top: 30px;
   }
+  &-meta {
+    .date {
+      display: inline-block;
+      color: #707070;
+      margin-right: rem(10);
+    }
+  }
   &-contents {
-    h2 {
+    margin-top: rem(50);
+    line-height: 1.75;
+    /deep/ h2 {
       margin-top: rem(24);
       font-size: rem(24);
       + * {
         margin-top: rem(12);
       }
     }
-    h3 {
+    /deep/ h3 {
       font-size: rem(20);
+      margin-top: rem(12);
       + * {
         margin-top: rem(8);
       }
     }
-    p {
-      + * {
-        margin: rem(8);
+    /deep/ p {
+      + p {
+        margin-top: rem(8);
+      }
+    }
+    /deep/ ul {
+      margin-top: rem(12);
+      padding: 0;
+      li {
+        position: relative;
+        padding-left: rem(30);
+        line-height: 2.5;
+        &::before {
+          content: "\f0a4";
+          font-family: "Font Awesome 5 Free";
+          display: block;
+          position: absolute;
+          font-weight: 400;
+          font-size: 1.2em;
+          top: -3px;
+          left: 0;
+        }
       }
     }
   }
